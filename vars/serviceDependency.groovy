e51abe3d0a47f6b8a954146e81447abe673756db
James Crozier <38696414+jcrozier@users.noreply.github.com>
2020-01-09 12:58:51 +0000

    Update serviceDependency.groovy

--- vars/serviceDependency.groovy
+++ vars/serviceDependency.groovy
@@ -8,6 +8,8 @@
  **/
 def call(Map params) {
 
+def version = params.version ?: ""
+	
 	sh "mvn  -DgenerateBackupPoms=false -U versions:${version} -Dincludes=ie.bil.lifeandpensions:domain"
 	sh "mvn  -DgenerateBackupPoms=false -U versions:${version} -Dincludes=ie.bil.lifeandpensions:service-providers"
 }
