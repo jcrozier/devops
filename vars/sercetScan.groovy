ed2a52766497aace7e8031fce0815db104536bd6
James Crozier <38696414+jcrozier@users.noreply.github.com>
2020-01-10 14:05:59 +0000

    Add files via upload

--- vars/sercetScan.groovy
+++ vars/sercetScan.groovy
@@ -9,6 +9,7 @@
 def call(Map params) {
 
 	checkoutRepo(url: "https://github.com/awslabs/git-secrets.git", branch: "*/master", credentials: "github")
+	sh "ls -alh"
 	sh "make install"
 	sh "git secrets --scan"
 }
