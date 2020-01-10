00441a0a09638fd87b84ae659162ca8d3a5077ce
James Crozier <38696414+jcrozier@users.noreply.github.com>
2020-01-10 09:58:57 +0000

    Update owasp.groovy

--- vars/owasp.groovy
+++ vars/owasp.groovy
@@ -8,9 +8,12 @@
  **/
 def call(Map params) {
 
-    def dataDir = params.dataDir ?: ""
+    	def dataDir = params.dataDir ?: ""
 	def autoUpdate = params.autoUpdate ?: ""
+	def projectname = params.projectname ?: ""
+	//sh 'mvn org.owasp:dependency-check-maven:check -Dformat=XML -DdataDirectory=${dataDir} -DautoUpdate=${autoUpdate}'
+	//	      step([$class: 'DependencyCheckPublisher', unstableTotalAll: '0'])
 
-	sh 'mvn org.owasp:dependency-check-maven:check -Dformat=XML -DdataDirectory=${dataDir} -DautoUpdate=${autoUpdate}'
-		      step([$class: 'DependencyCheckPublisher', unstableTotalAll: '0'])
+	sh(‘mkdir -p build/owasp’)
+	dependencycheck additionalArguments: "--project ${projectname} --scan ./ --data /home/jenkins/security/owasp-nvd/ --out build/owasp/dependency-check-report.xml --format XML", odcInstallation: "DependencyCheck"
 }
